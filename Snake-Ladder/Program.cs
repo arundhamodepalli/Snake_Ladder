0fb0a152d32e447c3c1bf02ba5b26a66c577d4da
ammu72642668 <arundhamodepalli726426@gmail.com>
2023-02-23 15:49:28 +0530

    single player at start position 0

---
+++ Snake-Ladder/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Snake_Ladder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to Snake and Ladder");
+        }
+    }
+}
